993e17120b12e6e8b28ab565649ccf08b39169d2
Ron Sprenkels <ron.sprenkels@gmail.com>
2023-12-31 12:09:41 +0100

    2023 DAY 3 completed

--- 2023/03/aoc_03.py
+++ 2023/03/aoc_03.py
@@ -1,10 +1,11 @@
+from collections import defaultdict
 from typing import List
 import re
 
 
 
 def sol_part1(lines: List[str], part = 1) -> int:
-    def has_symbol_around(lines, x_range=(0,0), y_par=0):
+    def search_symbol(lines, x_range=(0,0), y_par=0):
         def box_generator(xmin, xmax, y_par, w=len(lines[0]), h=len(lines)):
             for x in range(xmin - 1, xmax + 1):
                 for y in [y_par-1, y_par+1]:
@@ -19,10 +20,11 @@ def sol_part1(lines: List[str], part = 1) -> int:
         for x,y in box_generator(xmin, xmax, y_par):
             print(f'for xmin={xmin} xmax={xmax} y_par={y_par} checking {x},{y}')
             if (symbol := lines[y][x]) != '.' and not symbol.isdigit():
-                return True
-        return False
+                return (True, symbol, (x,y))
+        return (False, None, None)
 
     sum_partnumbers = 0
+    star_numbers = defaultdict(list)
     for y, line in enumerate(lines):
         x = 0
         while x < len(line):
@@ -34,11 +36,17 @@ def sol_part1(lines: List[str], part = 1) -> int:
                     x += 1
                 number = int(line[start:x])
                 print(f'found {number} at y={y} x=[{start}:{x}]')
-                if has_symbol_around(lines, x_range=(start, x), y_par=y):
+                has_symbol, the_symbol, location = search_symbol(lines, x_range=(start, x), y_par=y)
+                if has_symbol:
                     sum_partnumbers += number
+                    if the_symbol == '*':
+                        star_numbers[location].append(number)
                 else:
                     print(f'{number} does not count')
-    return sum_partnumbers
+    if part == 1:
+        return sum_partnumbers
+    else:
+        return sum([star_numbers[k][0] * star_numbers[k][1] for k in list(star_numbers.keys()) if len(star_numbers[k]) == 2])
 
 def sol_part2(lines):
     return sol_part1(lines, part=2)
@@ -58,6 +66,23 @@ def test_01():
 .664.598..
 """[1:-1].split('\n')
     assert sol_part1(lines) == 4361
+
+def test_02():
+    lines = """
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+"""[1:-1].split('\n')
+    assert sol_part2(lines) == 467835
+
+
 
 if __name__ == '__main__':
     lines = list(open('input.txt').read().splitlines())
